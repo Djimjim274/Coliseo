411428dd97fdc34e3dbce4c11d76ab5d72fa1b8f
djimjim274 <djimjim274@iesmartinezm.es>
2023-11-10 15:44:31 +0100

    admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Familia;
+use App\Entity\Producto;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        //return parent::index();
+        return $this->render('admin.html.twig');
+       // $routeBuilder = $this->get(CrudUrlGenerator::class)->build();
+       // return $this->redirect($routeBuilder->setController(FamiliaCrudController::class)->generateUrl());
+        
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template t odisplay a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+       //  return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Coliseo Djj');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Familia', 'icon class', Familia::class);
+        yield MenuItem::linkToCrud('Producto', 'icon class', Producto::class);
+    }
+}
